411f741f03bafde3b4e5437e27739a9335b94b85
GabeRomeroVY <gabe.romero@vidyard.com>
2021-01-30 07:24:36 -0500

    Delete test.sql
    Removing folder

--- test1/test.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table', tags=['publish']) }}
-
-SELECT 
-	account_master_table.vy_id,
-	account_master_table.sfdc_id
-FROM
-	{{ ref('account_id_master') }} account_master_table
-WHERE
-	account_master_table.sfdc_id IS NOT NULL
