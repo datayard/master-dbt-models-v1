c6eb2ebe6072f7ea7a94a263a7a462b6c5f0ce48
GabeRomeroVY <gabe.romero@vidyard.com>
2021-03-12 13:21:10 -0500

    Update stg_salesforce_accounts.sql
    fixing salesforce source

--- models/staging/salesforce/stg_salesforce_accounts.sql
+++ models/staging/salesforce/stg_salesforce_accounts.sql
@@ -3,7 +3,7 @@ with source as (
     select 
     	sfdc_account.id,
     	sfdc_account.name
-    from {{ source('salesforce_production', 'account') }}
+    from {{ source('salesforce', 'account') }}
         where true
 
 ),
