35503b150366806648db96f96d739794cd51d87d
kerwellr <81570627+kerwellr@users.noreply.github.com>
2021-04-23 10:28:37 -0400

    Zuora folder & tables (#15)
    * Added Staging Opportunity table along with docs in yml
    
    * Added Leads & Campaign Member tables
    
    * Added Leads & Campaign Member tables
    
    * Updated Vidyard Pubic tables & docs
    
    * Updated Vidyard Public tables and docs
    
    * Created Zuora staging folder & account, contact and subscription tables
    
    * Added Zuora folder & tables

---
+++ models/staging/zuora/stg_zuora_account.sql
@@ -0,0 +1,23 @@
+SELECT 
+        account.id as accountId,
+        account.account_number as accountNumber,
+        account.created_by_id as createdById,
+        account.created_date as createdDate,
+        account.crm_id as crmId,
+        account.customer_acquisition_date_c as customerAcquisitionDate,
+        account.cx_department_c as cxDepartment,
+        account.mrr as net_mrr,
+        account.name as accountName,
+        account.sales_rep_name as salesRepName,
+        account.vidyard_id_c as vidyardId,
+        account.sold_to_contact_id as soldToContactId,
+        account.bill_to_contact_id as billToContactId,
+        account.updated_by_id as updatedById,
+        account.updated_date as updatedDate,
+        account._fivetran_deleted as fivetranDeleted,
+        account._fivetran_synced as fivetranSynced
+FROM
+        {{ source ('zuora', 'account')}} as account
+
+WHERE
+        TRUE
