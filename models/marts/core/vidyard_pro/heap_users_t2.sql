f1b4a3aa1f1e6aad07418010532ca51bd4ab52a0
Suresh Subramani <82394314+datayard@users.noreply.github.com>
2021-06-23 05:49:28 -0400

    Dev dbt suresh (#47)
    * added heap_users_t2.sql and updated vidyard_users_t2.sql

---
+++ models/marts/core/vidyard_pro/heap_users_t2.sql
@@ -0,0 +1,10 @@
+select 
+	hp_users.userid as heap_user_id
+	, hp_users."identity"
+    , hp_video_started.userid as video_started_heap_id
+    , hp_video_finished.userid as video_finished_heap_id
+from {{ ref('stg_govideo_production_users') }} hp_users
+left join (select distinct userid from {{ ref('stg_govideo_production_video_creation_started_to_create_or_upload_a_video_combo') }}) hp_video_started
+	on hp_video_started.userid = hp_users.userid
+left join (select distinct userid from {{ ref('stg_govideo_production_video_recorded_or_uploaded') }}) hp_video_finished
+	on hp_video_finished.userid = hp_users.userid
