608f49928d1a41a4df75bdaab09d0934f18cc9b9
Suresh Subramani <82394314+datayard@users.noreply.github.com>
2021-07-10 19:45:03 -0400

    Dev dbt suresh (#82)
    * added derived field to the vidyard staging tables - users & organizations.

--- models/staging/vidyard/stg_vidyard_organizations.sql
+++ models/staging/vidyard/stg_vidyard_organizations.sql
@@ -11,5 +11,22 @@ SELECT
 	vidyard_organizations.locked_at as lockedAt,
 	vidyard_organizations.account_id as accountId,
 	vidyard_organizations.paying as paying
+	, case
+		when vidyard_organizations.created_by_client_id = 'android.vidyard.com'
+			then 'Android'
+		when vidyard_organizations.created_by_client_id = 'govideo-mobile.vidyard.com'
+			then 'iOS'
+		when vidyard_organizations.created_by_client_id = 'app.slack.com'
+			then 'Slack'
+		when vidyard_organizations.created_by_client_id = 'edge-extension.vidyard.com'
+			then 'Edge'
+		when vidyard_organizations.created_by_client_id = 'Enterprise'
+			then 'Enterprise'
+		when vidyard_organizations.created_by_client_id IN ('app.hubspot.com','marketing.hubspot.com','marketing.hubspotqa.com')
+			then 'Hubspot'
+		when vidyard_organizations.created_by_client_id NOT IN ('secure.vidyard.com','auth.viewedit.com','edge-extension.vidyard.com')
+			then 'Partner App'
+		else null
+	end as signup_source
  FROM 
 	{{ source('public', 'vidyard_organizations') }} as vidyard_organizations
