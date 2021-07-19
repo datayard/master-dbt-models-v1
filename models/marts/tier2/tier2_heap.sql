98fce2be66d61e229b3da60bd8179fa78a88d5ad
Suresh Subramani <82394314+datayard@users.noreply.github.com>
2021-07-19 19:40:17 -0400

    Dev dbt suresh (#89)
    * Added yml & sql files for Vidyard tables
    
    Added yml & sql files for Vidyard tables
    
    * Added yaml & sql files for the public tables
    
    * added a macro call to the project yml file 'on-run-end' to grant permission to looker
    
    * added grant permission to dbt. TODO: Grant permission code will eventually converted to Jinja that way it will be just one file for all the permissions
    
    * committing the auto generated files that are part of compilation process
    
    * yml file -> on-run-end -> changed the order of execution - dbt permission should precede looker
    
    * committing the auto generated files
    
    * +Added looker & dbt access to govideo_production tables. Note: These access permission will be eventually converted to jinja driven
    
    * Removed 'on-run-end' node from the project yml file for permissions testing
    
    * Added SHARING_SHARE_COMBO table
    
    * - removed obsolete macros
    
    * removed the duplicate column 'nonContract' from the salesforce_account table
    
    * compilation failure: reverting the changes made my Jinu
    
    * removed the files that caused the compilation failure
    
    * Test commit - Jinu GitHub walkthrough
    
    * created zuora t2 table - denormalized version of all the zuora tables
    
    * draft version of vidyard_user object
    
    * added heap_users_t2.sql and updated vidyard_users_t2.sql
    
    * created new table with user classification logic
    
    * updated vidyard_users_t2.sql
    
    * deleted test file vidyard_users.sql
    
    * added a field to the vid_users_t2
    
    * added product funnel kube
    
    * updated vid_users_t2 & added heap_sessions_t2.sql
    
    * fixed a variable name to be in sync with the naming convention
    
    * * updated vidyard_user_t2.sql
    
    * removed salesforce_account filters - not meant for the reporting.
    
    * fixed variable names in salesforce_contact.sql
    
    * heap obj changes - ignored the null 'vid_userid' records
    
    * fixed product_funnel_kube.sql based on the changes done in the heap objects
    
    * t2 table - heap light version
    
    * added dervied columns
    
    * removed obsolete heap_session object and updated the vidyard_users_t2.sql file
    
    * changed table names to ref in vidyard_users_t2.sql
    
    * fixed variable in govideo's sharing_share_combo table in heap_lv_t2.sql
    
    * added data from two more heap tables to the heap_lv_t2 table
    
    * removed an obsolete file: heap_user_t2.sql
    
    * removed the product_funnel_kube.sql
    
    * added vidyard_chapters table to the staging tier
    
    * removed heap_lv_t2.sql as this was added for testing. It performed well to the requirements. Removing it until run threshold for the object is determined.
    
    * removed vidyard_users_t2.sql as the newer version is available and for time being, the newer version will used to create the table on the dbt_vidyard_master directly without dbt.
    
    * added column 'sku' to the zuora_t2
    
    * added derived field to the vidyard staging tables - users & organizations.
    
    * added vidyard & viewedit email flags
    
    * committing all the tier2 files except Heap.
    
    * major changes: created tier2_heap table that run incremental on subsequent runs; added 4 kubes to the new kubes folder under model;
    
    * fixed heap sources tables with ref names

---
+++ models/marts/tier2/tier2_heap.sql
@@ -0,0 +1,239 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+
+SELECT
+        gs.eventid
+        , u."identity" AS vid_userid
+        , gs.userid
+        , gs.sessionid
+        , gs.sessiontime
+        , NULL         AS eventtime
+        , gs.landingpage
+        , gs.domain
+        , gs.channels
+        , gs.path
+        , CASE
+               WHEN gs.landingpage LIKE '%share.vidyard.com%'
+                   THEN 'Product'
+               WHEN gs.landingpage LIKE '%welcome.vidyard.com/chrome_setup/%'
+                   THEN 'Chrome'
+               WHEN gs.landingpage LIKE '%jiihcciniecimeajcniapbngjjbonjan%'
+                   THEN 'Chrome'
+               WHEN gs.channels LIKE 'Product'
+                   THEN 'Player'
+               ELSE gs.channels
+          END AS derived_channel
+        , gs.sessiontime AS column_for_distribution
+        , 'global_session'  AS tracker
+    FROM
+        {{ ref('stg_govideo_production_global_session') }} gs
+            JOIN {{ ref('stg_govideo_production_users') }} u
+                ON gs.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE gs.sessiontime > (select max(gs.sessiontime) from {{ this }})
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        oe.eventid
+        , u."identity" AS vid_userid
+        , oe.userid
+        , null AS sessionid
+        , null AS sessiontime
+        , oe.eventtime
+        , oe.landingpage
+        , oe.domain
+        , oe.channels
+        , oe.path
+        , null AS derived_channel
+        , oe.eventtime AS column_for_distribution
+        , 'opened_extension' AS tracker
+    FROM
+        {{ ref('stg_govideo_production_opened_extension') }} oe
+            JOIN {{ ref('stg_govideo_production_users') }} u
+                ON oe.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE oe.eventtime > (select max(oe.eventtime) from {{ this }})
+    {% endif %}
+
+
+    UNION ALL
+
+    SELECT
+        pv.eventid
+            , u."identity" AS vid_userid
+            , pv.userid
+            , NULL AS sessionid
+            , NULL AS sessiontime
+            , pv.eventtime
+            , pv.landingpage
+            , pv.domain
+            , NULL AS channels
+            , pv.path
+            , NULL AS derived_channel
+            , pv.eventtime AS column_for_distribution
+            , 'page_views' AS tracker
+    FROM
+        {{ ref('stg_govideo_production_pageviews') }} pv
+            JOIN {{ ref('stg_govideo_production_users') }} u
+                ON pv.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE pv.eventtime > (select max(pv.eventtime) from {{ this }})
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        ps.eventid
+        , u."identity" AS vid_userid
+        , ps.userid
+        , NULL AS sessionid
+        , NULL AS sessiontime
+        , ps.eventtime
+        , ps.landingpage
+        , ps.domain
+        , ps.channels
+        , ps.path
+        , NULL AS derived_channel
+        , ps.eventtime AS column_for_distribution
+        , 'product_sessions' AS tracker
+    FROM
+        {{ ref('stg_govideo_production_product_sessions') }} ps
+            JOIN {{ ref('stg_govideo_production_users') }} u
+                ON ps.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE ps.eventtime > (select max(ps.eventtime) from {{ this }})
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        ssc.eventid
+        , u."identity" AS vid_userid
+        , ssc.userid
+        , ssc.sessionid
+        , ssc.sessiontime
+        , ssc.eventtime
+        , ssc.landingpage
+        , ssc.domain
+        , ssc.channels
+        , ssc.path
+        , NULL AS derived_channel
+        , ssc.eventtime AS column_for_distribution
+        , 'sharing_share_combo' AS tracker
+    FROM
+        {{ ref('stg_govideo_production_sharing_share_combo') }} ssc
+        JOIN {{ ref('stg_govideo_production_users') }} u
+            ON ssc.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE ssc.eventtime > (select max(ssc.eventtime) from {{ this }})
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        vidcompv.eventid
+        , u."identity" AS vid_userid
+        , vidcompv.userid
+        , NULL AS sessionid
+        , NULL AS sessiontime
+        , vidcompv.eventtime
+        , vidcompv.landingpage
+        , vidcompv.domain
+        , vidcompv.channels
+        , vidcompv.path
+        , NULL AS derived_channel
+        , vidcompv.eventtime AS column_for_distribution
+        , 'vy_com_page_view' AS tracker
+    FROM
+        {{ ref('stg_govideo_production_vidyard_com_any_pageview') }} vidcompv
+            JOIN {{ ref('stg_govideo_production_users') }} u
+                ON vidcompv.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE vidcompv.eventtime > (select max(vidcompv.eventtime) from {{ this }})
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        vidcomss.eventid
+        , u."identity" AS vid_userid
+        , vidcomss.userid
+        , vidcomss.sessionid
+        , vidcomss.sessiontime
+        , NULL AS eventtime
+        , vidcomss.landingpage
+        , vidcomss.domain
+        , vidcomss.channels
+        , vidcomss.path
+        , NULL AS derived_channel
+        , vidcomss.sessiontime AS column_for_distribution
+        , 'vy_com_sessions' AS tracker
+    FROM {{ ref('stg_govideo_production_vidyard_com_sessions') }} vidcomss
+        JOIN {{ ref('stg_govideo_production_users') }} u
+            ON vidcomss.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE vidcomss.sessiontime > (select max(vidcomss.sessiontime) from {{ this }})
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        pv.eventid
+        , u."identity" AS vid_userid
+        , pv.userid
+        , NULL AS sessionid
+        , NULL AS sessiontime
+        , pv.eventtime
+        , pv.landingpage
+        , pv.domain
+        , NULL AS channels
+        , pv.path
+        , NULL AS derived_channel
+        , pv.eventtime AS column_for_distribution
+        , 'video_creation' AS tracker
+    FROM
+        {{ ref('stg_govideo_production_video_creation_started_to_create_or_upload_a_video_combo') }} pv
+            JOIN {{ ref('stg_govideo_production_users') }} u
+                ON pv.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE pv.eventtime > (select max(pv.eventtime) from {{ this }})
+    {% endif %}
+
+    UNION ALL
+
+    SELECT
+        pv.eventid
+        , u."identity" AS vid_userid
+        , pv.userid
+        , NULL AS sessionid
+        , NULL AS sessiontime
+        , pv.eventtime
+        , pv.landingpage
+        , pv.domain
+        , NULL AS channels
+        , pv.path
+        , NULL AS derived_channel
+        , pv.eventtime AS column_for_distribution
+        , 'video_upload' AS tracker
+    FROM
+        {{ ref('stg_govideo_production_video_recorded_or_uploaded') }} pv
+            JOIN {{ ref('stg_govideo_production_users') }} u
+                ON pv.userid = u.userid AND u."identity" IS NOT NULL
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE pv.eventtime > (select max(pv.eventtime) from {{ this }})
+    {% endif %}
