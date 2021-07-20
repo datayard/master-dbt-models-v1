27cd844a15bbb04eca975b492216e98d1b728172
Suresh Subramani <suresh.subramani@vidyard.com>
2021-07-20 21:40:53 +0000

    updated 'vidyard_heap_entities' kube

--- models/kubes/kube_vidyard_heap_entities.sql
+++ models/kubes/kube_vidyard_heap_entities.sql
@@ -1,6 +1,57 @@
-SELECT entity
-         , vet2.userid
-         , vet2.organizationid
+SELECT 
+         --USER
+        kbvzu.userid
+        , email
+        , vidyard_email
+        , viewedit_email
+        , domain_type
+
+        --ORGANIZATION
+        , kbvzu.organizationid
+        , ownerid
+        , org_accountid
+        , orgtype
+        , parentid
+        , org_name
+        , org_createddate
+        , org_updateddate
+        , org_createdbyclientid
+        , paying
+        , signup_source
+
+        --ORG_METRICS
+        , firstview
+        , firstviewvideoid
+        , totalseconds
+        , videoswithviews
+        , viewscount
+
+        --GROUP & TEAM
+        , groupid
+        , teamid
+
+        --CALCULATED FIELDS
+        , has_access_to_enterprise_account
+        , has_personal_account
+        , linked_to_parent_account
+        , user_type
+        , account_type --PARAMETER COMPUTED FROM ZUORA INFO
+
+        --ZUORA
+        , subscriptionid
+        , subscriptionstartdate
+        , subscriptionenddate
+        , zuoraaccountid
+        , zuoracrmid
+        , rateplanid
+        , productrateplanid
+        , productid
+        , net_mrr
+        , subscriptionstatus
+        , soldtocontactid
+
+         --Entity
+         , entity
          , entityid
          , childentityid
          , createddate
@@ -33,6 +84,7 @@ SELECT entity
          , derived_channel
          , column_for_distribution
          , tracker
-    FROM {{ ref('tier2_vidyard_user_entities') }} vet2
+         
+    FROM {{ ref('kube_vidyard_user_entities') }} kbvzu
              JOIN {{ ref('tier2_heap') }} ht2
-                  ON ht2.vid_userid = vet2.userid
+                  ON ht2.vid_userid = kbvzu.userid
