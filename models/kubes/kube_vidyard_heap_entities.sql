ab798dccc81d277b29d186b795535c39ce4d258e
Suresh Subramani <suresh.subramani@vidyard.com>
2021-07-30 20:06:22 +0000

    kubes commit for validation comments

--- models/kubes/kube_vidyard_heap_entities.sql
+++ models/kubes/kube_vidyard_heap_entities.sql
@@ -1,90 +1,91 @@
 SELECT 
-         --USER
-        kbvzu.userid
-        , email
-        , vidyard_email
-        , viewedit_email
-        , domain_type
+     --USER
+     kbvzu.userid
+    , email
+    , email_to_exclude
+    , kbvzu.domain
+    , domain_type
 
-        --ORGANIZATION
-        , kbvzu.organizationid
-        , ownerid
-        , org_accountid
-        , orgtype
-        , parentid
-        , org_name
-        , org_createddate
-        , org_updateddate
-        , org_createdbyclientid
-        , paying
-        , signup_source
+     --ORGANIZATION
+     , kbvzu.organizationid
+     , ownerid
+     , org_accountid
+     , orgtype
+     , parentid
+     , org_name
+     , org_createddate
+     , org_updateddate
+     , org_createdbyclientid
+     , paying
+     , signup_source
 
-        --ORG_METRICS
-        , firstview
-        , firstviewvideoid
-        , totalseconds
-        , videoswithviews
-        , viewscount
+     --ORG_METRICS
+     , firstview
+     , firstviewvideoid
+     , totalseconds
+     , videoswithviews
+     , viewscount
 
-        --GROUP & TEAM
-        , groupid
-        , teamid
+     --GROUP & TEAM
+     , groupid
+     , teamid
 
-        --CALCULATED FIELDS
-        , has_access_to_enterprise_account
-        , has_personal_account
-        , linked_to_parent_account
-        , user_type
-        , account_type --PARAMETER COMPUTED FROM ZUORA INFO
+     --CALCULATED FIELDS
+     , has_access_to_enterprise_account
+     , has_personal_account
+     , linked_to_parent_account
+     , user_type
+     , account_type --PARAMETER COMPUTED FROM ZUORA INFO
 
-        --ZUORA
-        , subscriptionid
-        , subscriptionstartdate
-        , subscriptionenddate
-        , zuoraaccountid
-        , zuoracrmid
-        , rateplanid
-        , productrateplanid
-        , productid
-        , net_mrr
-        , subscriptionstatus
-        , soldtocontactid
+     --ZUORA
+     , subscriptionid
+     , subscriptionstartdate
+     , subscriptionenddate
+     , zuoraaccountid
+     , zuoracrmid
+     , rateplanid
+     , productrateplanid
+     , productid
+     , net_mrr
+     , subscriptionstatus
+     , soldtocontactid
 
-         --Entity
-         , entity
-         , entityid
-         , childentityid
-         , createddate
-         , updateddate
-         , type
-         , createdbyclientid
-         , uuid
-         , origin
-         , derived_origin
-         , source
-         , status
-         , issecure
-         , milliseconds
-         , inviteaccepted
-         , userscore
-         , usercomment
-         , filled
-         , allowcontact
-         , cancelled
-         , eventid
-         , vid_userid
-         , ht2.userid AS heapuserid
-         , sessionid
-         , sessiontime
-         , eventtime
-         , landingpage
-         , domain
-         , channels
-         , path
-         , derived_channel
-         , column_for_distribution
-         , tracker
-         
-    FROM {{ ref('kube_vidyard_user_entities') }} kbvzu
-             JOIN {{ ref('tier2_heap') }} ht2
-                  ON ht2.vid_userid = kbvzu.userid
+     --Entity
+     , entity
+     , entityid
+     , childentityid
+     , createddate
+     , updateddate
+     , type
+     , createdbyclientid
+     , uuid
+     , origin
+     , derived_origin
+     , source
+     , status
+     , issecure
+     , milliseconds
+     , inviteaccepted
+     , userscore
+     , usercomment
+     , filled
+     , allowcontact
+     , cancelled
+     , eventid
+     , vid_userid
+     , ht2.userid AS heapuserid
+     , sessionid
+     , sessiontime
+     , eventtime
+     , landingpage
+     , ht2.domain as heap_domain
+     , channels
+     , path
+     , derived_channel
+     , column_for_distribution
+     , tracker
+     
+FROM {{ ref('kube_vidyard_user_entities') }} kbvzu
+          JOIN {{ ref('tier2_heap') }} ht2
+               ON   ht2.vy_userid = kbvzu.userid
+                    AND ht2.is_vy_userid_integer = 1
