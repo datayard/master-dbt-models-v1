3f29a10bebd0eddda9fc582e91680004837e5c32
Gabe Romero <58733497+GabeRomeroVY@users.noreply.github.com>
2021-08-09 10:35:59 -0400

    Dev dbt gabe (#108)
    * adding the logic to calculate the use case in accounts according to the opportunity fields
    
    * adding the missing columns
    
    * Fixing the usecase name
    
    * again I keep missing the table name
    
    * didn't save....
    
    * fixed the group by reference and permissions
    
    * adding the nps survey table
    
    * adding the NPS surveys calculation
    
    * remove the 7 day filer
    
    * Filtering out the TNPS
    
    * updating the name for subscriptions and starting the counter
    
    * Starting the Customer count calculator on to DBT from ZuoraData
    
    * adding the SKU and status filters
    
    * adding the status on Account and other changes
    
    * adding the paying flag on the vy_orgs table
    
    * Adding the Active Pro Subscriptions file
    
    * Adding the VY account ID to the pro accounts model
    
    * removing non-Pro accounts from the Cancel filter
    
    * im Adding the Master user ID model
    
    * changing the materialization of Marts into tables
    
    * adding the survey type field into the model
    
    * fixing the utils.yml
    
    * Second fix for the yml file, test doesn't fail!
    
    * The main dbt_proyect.yml had an issue on the name in which the configuration was ignored
    
    * fix materialization issue
    
    * delete target folder
    
    * fixing the duplicate columns
    
    * fixed the duplicate names on the tier2 zuora table
    
    * adding the referrals code and organizing the Tier 2 and Kubes tables
    
    * created folder for tier 2 and moved the Zuora tables
    
    * Fixed the source on referrals
    
    * adding Intercom staging models
    
    * Fixed the format on the intercom YML file
    
    * finishing the Intercom models
    
    * adjusted the sources to map the views
    
    * adjusted names and fixed tables for ID's
    
    * Adding the customer counter code main SQL to be referenced
    
    * Adding couple changes to the customer_count

--- models/marts/utils/fct_customer_counter.sql
+++ models/marts/utils/fct_customer_counter.sql
@@ -1,40 +1,59 @@
-WITH pro_acocunts as (
+WITH pro_acccounts as (
     SELECT
-        'Self-serve' as accountype,
+        'Self-serve' as accounttype,
         COUNT (distinct Pro_accounts.activeproaccounts) as accounts
     FROM
-         {{ ref('fct_active_pro_subscriptions') }} as Pro_accounts
+        {{ ref('fct_active_pro_subscriptions') }} as Pro_accounts
+        --dbt_vidyard_master.fct_active_pro_subscriptions as Pro_accounts
     WHERE
           Pro_accounts.name NOT LIKE 'Free Forever'
-    GROUP BY
-        accountype
     ),
 
-    customer_accounts as (
+    commercial_accounts as (
     SELECT
-        account.employeesegment as accountype,
+        'commercial_accounts' as accounttype,
         COUNT (distinct account.accountid) as accounts
     FROM
         {{ ref('stg_salesforce_account') }} as account
+        --dbt_vidyard_master.stg_salesforce_account as account
     WHERE
         account.accounttype LIKE 'Customer'
+        AND account.employeesegment LIKE 'Commercial'
         AND account.ispersonaccount = false
-    GROUP BY
-        accountype
+    ),
+
+    emerging_accounts as (
+    SELECT
+        'emerging_accounts' as accounttype,
+        COUNT (distinct account.accountid) as accounts
+    FROM
+        {{ ref('stg_salesforce_account') }} as account
+        --dbt_vidyard_master.stg_salesforce_account as account
+    WHERE
+        account.accounttype LIKE 'Customer'
+        AND account.employeesegment NOT LIKE 'Commercial'
+        AND account.ispersonaccount = false
     )
+
 SELECT
-       accountype,
-       accounts
+    *
 FROM
     (
-        SELECT accountype,
-               accounts
-        FROM pro_acocunts
+        SELECT
+            accounttype,
+            accounts
+        FROM pro_acccounts
         UNION ALL
-        SELECT accountype,
-               accounts
-        FROM customer_accounts
+        SELECT
+            accounttype,
+            accounts
+        FROM commercial_accounts
+        UNION ALL
+        SELECT
+            accounttype,
+            accounts
+        FROM emerging_accounts
     )
 GROUP BY
-    accountype,
+    accounttype,
     accounts
