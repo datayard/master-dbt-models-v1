143f36439d57667b1664ef8fad0c929d472fd914
Suresh Subramani <suresh.subramani@vidyard.com>
2021-08-09 20:03:19 +0000

    new classification logic

---
+++ models/marts/tier2/tier2_user_teams_folders.sql
@@ -0,0 +1,42 @@
+SELECT
+    u.userid
+    , o.ownerid
+    , o.organizationid
+    , o.accountid
+    , o.parentid
+    , o.orgtype
+    , u.email
+    , CASE
+        WHEN o.orgtype = 'self_serve' and o.organizationid = o.accountid
+            THEN 'personal'
+        WHEN o.orgtype IS NULL and o.organizationid != o.accountid
+            THEN 'shared'
+        WHEN o.orgtype IS NULL and o.organizationid = o.accountid
+            THEN 'parent folder'
+        WHEN o.orgtype = 'self_serve' and o.organizationid != o.accountid
+            THEN 'personal enterprise'
+        WHEN o.orgtype IS NULL and o.organizationid IS NULL
+            THEN 'orphan'
+        ELSE NULL
+    END AS folder_type
+FROM dbt_vidyard_master.stg_vidyard_users u
+        LEFT JOIN dbt_vidyard_master.stg_vidyard_organizations o
+                ON o.ownerid = u.userid
+
+UNION ALL
+
+SELECT
+    u.userid
+    , NULL AS ownerid
+    , NULL AS organizationid
+    , accountid
+    , NULL AS parentid
+    , NULL AS orgtype
+    , u.email
+    , 'parent folder' as folder_type
+FROM
+    dbt_vidyard_master.stg_vidyard_users u
+    JOIN dbt_vidyard_master.stg_vidyard_team_memberships tm
+        ON tm.userid = u.userid and tm.inviteaccepted = true
+    JOIN dbt_vidyard_master.stg_vidyard_teams t
+        ON t.teamid = tm.teamid
