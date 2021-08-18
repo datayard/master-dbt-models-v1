5a6ff129658174d7b44a7bab21ed2d15a10381d5
Jodi Zhang <jodi.zhang@vidyard.com>
2021-08-18 16:38:57 +0000

    updated heap stg, tier2 and kube to add use case

--- models/staging/heap/stg_govideo_production_users.sql
+++ models/staging/heap/stg_govideo_production_users.sql
@@ -1,7 +1,7 @@
 SELECT
         users.user_id as userID,
         users."identity" as identifier,
-        --cast("identity" as varchar(10)) as vidyardUserId
+        cast("identity" as varchar(10)) as vidyardUserId,
         users.appcuesuserid as appcuesUserID,
         users.joindate as joinDate,
         users.last_modified as lastModifiedDate,
@@ -15,7 +15,21 @@ SELECT
         users.mirrored_camera as mirroredCamera,
         users.general_use_case as generalUseCase,
         users.specific_use_case as specificUseCase,
-        users.tailored_onboarding_use_case_response as onboardingUsecaseResponse
+        users.tailored_onboarding_use_case_response as onboardingUsecaseResponse, 
+        users.usecase,
+        case
+                when users.usecase ilike '%skip%' then null
+                when users.usecase ilike '%customer%' then 'customer-success'
+                when users.usecase ilike '%other%' and users.general_use_case is not null and users.general_use_case not ilike'%skip%' then null
+                else users.usecase
+        end as usecase_c, 
+        case
+                when users.general_use_case ilike '%skip%' then null
+                when users.general_use_case ilike '%customer%' then 'customer-success'
+                when users.general_use_case ilike '%other%' and users.usecase is not null and users.usecase not ilike'%skip%' then null
+                else users.general_use_case
+        end as general_use_case_c, 
+        lower(coalesce(general_use_case_c,usecase_c)) as combined_usecase     
 
 FROM
         {{ source('govideo_production' , 'users')}} as users
