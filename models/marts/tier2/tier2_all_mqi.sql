aef337e898cea5fe55cb090f59f5f37e3276aa49
Gladwin Lee <glee@Gladwins-MacBook-Pro.local>
2021-09-14 01:51:49 -0400

    created all_mqi parent

--- models/marts/tier2/tier2_all_mqi.sql
+++ models/marts/tier2/tier2_all_mqi.sql
@@ -1,24 +1,57 @@
-SELECT u.leadid,
-       u.email,
-       u.domaintype,
-       cm.createddate,
-       cm.createddate as mqi_date,
-       cm.mql_date_and_time__c as mql_date,
-       cm.mql__c,
-       cm.sal__c,
-       cm.sal_date_and_time__c as sal_date,
-       cm.sql__c,
-       cm.sql_date_and_time__c as sql_date,
-       cm.sqo__c,
-       cm.sqo_date__c as sqo_date,
-       cm.won__c,
-       cm.opportunity_closed_won_date__c as won_date,
-       cm.campaign_sourced_by__c,
-       cm.id as parent_campaign_id,
-       cm.cta_subtype__c
+SELECT
+        u.leadid
+      , u.email
+      , u.domainType
+      , cm.createdDate as mqi_date
+      , cm.mqlDate as mql_date
+      , cm.mql
+      , cm.sal
+      , cm.salDate
+      , cm.sql
+      , cm.sqlDate
+      , cm.sqo
+      , cm.sqoDate
+      , cm.won
+      , cm.opportunityClosedWonDate
+      , cm.campaignSourcedBy
+      , c.campaignid as parentCampaignId
+      , coalesce(c.campaignName, cm.campaign_name) as campaign
+      , coalesce(c.cta, cm.campaign_cta) as cta_type
+      , coalesce (c.channelPicklist, cm.campaign_channelpicklist) as channel
+      , c.campaign_ctasubtype
 FROM {{ ref('tier2_salesforce_lead') }} as u
 JOIN {{ ref('tier2_salesforce_campaign_and_members') }} as cm
     ON cm.leadid = u.leadid
 LEFT JOIN {{ ref('stg_salesforce_campaign')}} as c
     ON c.campaignId = cm.campaign_parentid
-
+WHERE
+      u.isconverted = 'false' -- need to ignore leads that were converted to contact
+UNION
+SELECT
+        u.contactId
+      , u.email
+      , u.domainType
+      , cm.createdDate as mqi_date
+      , cm.mqlDate as mql_date
+      , cm.mql
+      , cm.sal
+      , cm.salDate
+      , cm.sql
+      , cm.sqlDate
+      , cm.sqo
+      , cm.sqoDate
+      , cm.won
+      , cm.opportunityClosedWonDate
+      , cm.campaign_sourcedby
+      , c.campaignid as parentCampaignId
+      , coalesce(c.campaignName, cm.campaign_name) as campaign
+      , coalesce(c.cta, cm.campaign_cta) as cta_type
+      , coalesce (c.channelPicklist, cm.campaign_channelpicklist) as channel
+      , c.ctaSubtype
+FROM {{ ref('tier2_salesforce_contact') }} as u
+JOIN {{ ref('tier2_salesforce_campaign_and_members') }} as cm
+    ON cm.contactId = u.contactId
+LEFT JOIN {{ ref('stg_salesforce_campaign') }} as c
+    ON c.campaignId = cm.campaign_parentid
+JOIN {{ ref('tier2_salesforce_account') }} as a
+    ON a.accountId = u.accountId
