199ed402c493bd6ec13aa60f1c480a64e0278d2a
Jodi Zhang <75333353+JodiZhangV@users.noreply.github.com>
2021-09-15 10:33:53 -0400

    Dev dbt jodi (#191)
    * testing commiting kube for usecase segmentation
    
    * updated use case segmentation query
    
    * added a new kube for vidyard users to get signup source
    
    * updated kube user detail to remove dupes
    
    * adding activated flag
    
    * removing activated flag, will add in stg_org_metrics
    
    * adding activated flag
    
    * updated column names
    
    * updated heap stg, tier2 and kube to add use case
    
    * uncommenting vy_userid
    
    * uncommented vy_userid
    
    * fixed user detail kube
    
    * updated heap model
    
    * cast activated flag as boolean
    
    * update combined use case data
    
    * updated activatedflag
    
    * added sessionid for pageviews heapnew
    
    * fixed domaintype name
    
    * added a flag for using chrome ext or created a video
    
    * added a new personal email domain to sf leads, contacts & vidyard users
    
    * fixed stg vidyard users, sf contacts and leads

--- models/staging/salesforce/stg_salesforce_lead.sql
+++ models/staging/salesforce/stg_salesforce_lead.sql
@@ -54,7 +54,7 @@ SELECT
               or split_part(sfdc_lead.email, '@', 2) like '%web.de'
               or split_part(sfdc_lead.email, '@', 2) like '%wp.pl'
               or split_part(sfdc_lead.email, '@', 2) like '%getnada.com'
-              or split_part(u.email, '@', 2) like '%tutanota.com%'
+              or split_part(sfdc_lead.email, '@', 2) like '%tutanota.com%'
                 then 'personal'
               when split_part(sfdc_lead.email, '@', 2) like '%.edu%'
               or split_part(sfdc_lead.email, '@', 2) like '%edsfdc_lead.%'
