927064d045aa7d64dcd344fe8ee8ae898c286e6f
Jodi Zhang <75333353+JodiZhangV@users.noreply.github.com>
2021-09-22 15:21:37 -0400

    Dev dbt jodi (#216)
    * testing commiting kube for usecase segmentation
    
    * updated use case segmentation query
    
    * added a new kube for vidyard users to get signup source
    
    * updated kube user detail to remove dupes
    
    * adding activated flag
    
    * removing activated flag, will add in stg_org_metrics
    
    * adding activated flag
    
    * updated column names
    
    * updated heap stg, tier2 and kube to add use case
    
    * uncommenting vy_userid
    
    * uncommented vy_userid
    
    * fixed user detail kube
    
    * updated heap model
    
    * cast activated flag as boolean
    
    * update combined use case data
    
    * updated activatedflag
    
    * added sessionid for pageviews heapnew
    
    * fixed domaintype name
    
    * added a flag for using chrome ext or created a video
    
    * added a new personal email domain to sf leads, contacts & vidyard users
    
    * fixed stg vidyard users, sf contacts and leads
    
    * filtering out the following classifications from the kube ('anomaly','orphan','unknown') - really isnt needed in looker and having to filter these add complexity to some joins
    
    * changes to team memberships so that empty teams will still appear
    
    * added new heap stging tables and updated the joins for events tier2

---
+++ models/staging/heap/stg_govideo_production_manage_combo.sql
@@ -0,0 +1,28 @@
+SELECT
+  manage_combo.event_id as eventID,
+  manage_combo.user_id as userID,
+  manage_combo.session_id as sessionID,
+  manage_combo.time as eventTime,
+  manage_combo.session_time as sessionTime,
+  --manage_combo.continent as continent,
+  manage_combo.country as country,
+  manage_combo.region as region,
+  manage_combo.city as city,
+  manage_combo.platform as platform,
+  manage_combo.device as device,
+  manage_combo.device_type as deviceType,
+  manage_combo.browser as browser,
+  --manage_combo.browser_type as browserType,
+  --manage_combo.vidyard_platform as vidyardPlatform,
+  manage_combo.referrer as referrer,
+  manage_combo.landing_page as landingPage,
+  manage_combo.landing_page_query as landingPageQuery,
+  manage_combo.query as query,
+  manage_combo.domain as domain,
+  manage_combo.path as path,
+  manage_combo.title as title,
+  manage_combo.channels as channels
+FROM
+        {{ source('govideo_production' ,'manage_combo') }} as manage_combo
+WHERE
+        TRUE
