c26a626b8fcfc56eeae622490e81c52ad62b3e88
Jinu Mathew P <jinu.mathew@vidyard.com>
2021-09-27 23:50:24 +0000

    Added user_id_master.sql

---
+++ models/marts/syncs/marketo/user_id_master.sql
@@ -0,0 +1,41 @@
+/*{{ config(materialized='table', tags=['publish']) }}*/
+
+WITH
+  sfdc_notconverted_leads as (
+    SELECT
+      sfdc_leads_og.leadId,
+      sfdc_leads_og.email
+    FROM  
+      {{ ref('stg_salesforce_lead') }} as sfdc_leads_og
+    WHERE
+      sfdc_leads_og.isConverted = 'False'
+  )
+
+SELECT
+  vy_users.email,
+  split_part(vy_users.email, '@', 2) as email_domain,
+  vy_users.userId as vy_user_ID,
+  sfdc_contacts.contactId as sfdc_contact_ID,
+  sfdc_leads.id as sfdc_lead_ID,
+  mkto_leads.leadId as mkto_lead_ID, 
+  heap_users.userID as heap_id
+FROM
+  {{ref('stg_vidyard_users')}} as vy_users
+LEFT JOIN
+  {{ref('stg_salesforce_contact')}} as sfdc_contacts
+ON
+  vy_users.email = sfdc_contacts.email
+LEFT JOIN
+  sfdc_notconverted_leads as sfdc_leads
+ON
+  vy_users.email = sfdc_leads.email
+LEFT JOIN
+  {{ref('stg_marketo_lead')}} as  mkto_leads
+ON
+  vy_users.email = mkto_leads.emailAddress
+LEFT JOIN
+  {{('stg_govideo_production_users')}} as  heap_users
+ON
+  vy_users.id = cast(heap_users.identifier as varchar(10))
+WHERE
+  true
