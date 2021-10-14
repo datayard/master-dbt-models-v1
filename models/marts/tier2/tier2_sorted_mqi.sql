d5e618ca972beeacec52b2510bc779cbe2298205
isaac.wu@vidyard.com <isaac.wu@vidyard.com>
2021-10-14 21:14:37 +0000

    rank mqi date for domain to count NLs

--- models/marts/tier2/tier2_sorted_mqi.sql
+++ models/marts/tier2/tier2_sorted_mqi.sql
@@ -33,7 +33,7 @@ SELECT
      /* , am.employeeSegment */
       , am.responseStatus
       , row_number() over(partition by am.email order by am.mqiDateGMT) as rn
-
+      , row_number() over(partition by am.domain order by am.mqiDateGMT) as domain_rn
       , case when am.persona like '%Decision Maker' then 'Decision Maker' 
              when (am.persona like '%Influencer' or am.persona like '%General') then 'Individual Contributor' 
              else 'Other' end as persona
