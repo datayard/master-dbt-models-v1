f8350f4fb74c24673e76a105e35cc4cfc07a1dd3
Jodi Zhang <jodi.zhang@vidyard.com>
2021-10-15 17:58:24 +0000

    updated users classification table to remove join to account id and removed commented fields from team memberships

--- models/marts/tier2/tier2_vidyard_team_memberships.sql
+++ models/marts/tier2/tier2_vidyard_team_memberships.sql
@@ -1,17 +1,13 @@
-SELECT                             
-    -- vud2.userid     
+SELECT                               
     tm.userid                             
     , t.accountid             
     , t.teamid                         
     , tm.teammembershipid    
     , tm.createddate                
-    , tm.updateddate as inviteaccepteddate               
-    -- , vud2.orgtype                       
+    , tm.updateddate as inviteaccepteddate                          
     , tm.inviteaccepted                            
     , t.isadmin                                    
 FROM
     {{ ref('stg_vidyard_teams') }} t
     LEFT JOIN {{ ref('stg_vidyard_team_memberships') }} tm
         ON t.teamid = tm.teamid
-    -- LEFT JOIN {{ ref('tier2_vidyard_user_details') }} vud2    
-    --     ON tm.userid = vud2.userid 
