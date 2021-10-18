db9a0ddb703d4d2fe1525586bd84a5f25942a5c8
Jodi Zhang <75333353+JodiZhangV@users.noreply.github.com>
2021-10-18 11:08:23 -0400

    Dev dbt jodi (#261)
    * testing commiting kube for usecase segmentation
    
    * updated use case segmentation query
    
    * added a new kube for vidyard users to get signup source
    
    * updated kube user detail to remove dupes
    
    * adding activated flag
    
    * removing activated flag, will add in stg_org_metrics
    
    * adding activated flag
    
    * updated column names
    
    * updated heap stg, tier2 and kube to add use case
    
    * uncommenting vy_userid
    
    * uncommented vy_userid
    
    * fixed user detail kube
    
    * updated heap model
    
    * cast activated flag as boolean
    
    * update combined use case data
    
    * updated activatedflag
    
    * added sessionid for pageviews heapnew
    
    * fixed domaintype name
    
    * added a flag for using chrome ext or created a video
    
    * added a new personal email domain to sf leads, contacts & vidyard users
    
    * fixed stg vidyard users, sf contacts and leads
    
    * filtering out the following classifications from the kube ('anomaly','orphan','unknown') - really isnt needed in looker and having to filter these add complexity to some joins
    
    * changes to team memberships so that empty teams will still appear
    
    * added new heap stging tables and updated the joins for events tier2
    
    * added vidyard_active_embeds
    
    * updated yml file for vidyard
    
    * commented out the filter that removes anomly and orphans, etc since that drops off users from the enterprise side that arent yet classified properly
    
    * removed join to user details from team memberships table since it cases duplicates and join issues in looker
    
    * fixed eventid to reference events rather than the left joined table eventjoins
    
    * updated employee segment
    
    * added org metrics data to kube org wide metrics
    
    * updated users classification table to remove join to account id and removed commented fields from team memberships
    
    * changes to kube org wide metrics sql to get only active PAID accounts based on zuora

--- models/kubes/kube_org_wide_metrics.sql
+++ models/kubes/kube_org_wide_metrics.sql
@@ -1,14 +1,24 @@
+with zuora_enterprise_accounts as (
+    select 
+        distinct zuora.vidyardAccountId
+    from {{ ref('tier2_zuora') }} zuora
+    where 
+        zuora.sku not in ('SS-010', 'SKU-00000009', 'SKU-00000020') 
+        and zuora.subscription_type  like '%Active%'
+)
 select
     o.organizationid
     , o.accountid
     , om.totalseconds as totalSeconds
     , om.viewscount as viewsCount
     , om.videoswithviews as videosWithViews
-    , count(distinct v.childentityid) as videos_count
+    , count(distinct case when v.origin != 'sample' then v.childentityid else null end) as videos_count
     , count(distinct e.eventid) as events_count
     , count(distinct case when e.eventjoinid is not null then e.eventid else null end) as applied_events_count
     , count(distinct case when e.eventtype not like '%simple%' then e.eventjoinid else null end) as custom_vidoes
 from {{ ref('stg_vidyard_organizations') }} o
+join zuora_enterprise_accounts z
+    on o.accountid = z.vidyardaccountid
 left join {{ ref('tier2_vidyard_videos') }} v
     on o.organizationid = v.organizationid
 left join {{ ref('tier2_vidyard_events') }} e
