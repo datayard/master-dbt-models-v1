2b9d93ab977252bcc9773839abee4e4561e8bf5e
omgboom <42044269+omgboom@users.noreply.github.com>
2021-11-30 12:53:18 -0500

    CustomerType Added
    CustomerType added to Opportunities and Subscriptions. Subscriptions is important since pro will no longer exist in Salesforce so you can't just join SFDC Accounts.

--- models/finkubes/finkube_subscriptions.sql
+++ models/finkubes/finkube_subscriptions.sql
@@ -16,7 +16,9 @@ select
   , fiscalyearmonth
   , fiscalquarter
   , fiscalyear
+  , case when a.ispersonaccount or a.isselfserve or a.ispersonaccount is null then 'Vidyard Pro' else a.employeesegment end as customertype
   , case when yearmonth <= '2020-11' then nvl(sarr,0) else nvl(zarr,0) end as arr
   , arr * 1.0 / 12 as mrr
 from zuora
 full outer join sfdc using (accountid, region, idtype, yearmonth, yearmonthvalue, fiscalyearmonth, fiscalquarter, fiscalyear)
+left join {{ref('tier2_salesforce_account')}} a using (accountid)
