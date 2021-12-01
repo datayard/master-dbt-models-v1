20775b28f201984595c1a37c73b72346205cb0b2
omgboom <42044269+omgboom@users.noreply.github.com>
2021-12-01 10:12:44 -0500

    Adding CustomerType, Fixing Formula Error

--- models/finkubes/finkube_subscriptions.sql
+++ models/finkubes/finkube_subscriptions.sql
@@ -18,7 +18,7 @@ select
   , fiscalyear
   , case when a.ispersonaccount or a.isselfserve or a.ispersonaccount is null then 'Vidyard Pro' else a.employeesegment end as customertype
   , case when yearmonth <= '2020-11' then nvl(sarr,0) else nvl(zarr,0) end as arr
-  , arr * 1.0 / 12 as mrr
+   , case when yearmonth <= '2020-11' then nvl(sarr/12,0) else nvl(zarr/12,0) end as mrr
 from zuora
 full outer join sfdc using (accountid, region, idtype, yearmonth, yearmonthvalue, fiscalyearmonth, fiscalquarter, fiscalyear)
 left join {{ref('tier2_salesforce_account')}} a using (accountid)
