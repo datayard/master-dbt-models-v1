e44c829e843a2012c55921c8b9b4b5ace0de38de
omgboom <42044269+omgboom@users.noreply.github.com>
2021-12-02 13:22:56 -0500

    Origin/dev dbt matt (#354)
    * Fixes
    
    Fixed Leads MQI Dates
    Fixed Pipeline & Pipeline Changes
    
    * Fix to Rate Plan Charge
    
    Fix Rate Plan Charge so that FinTier2 Zuora tables work.
    
    * Quick Change - Case When Error
    
    * Update finkube_opportunities.sql
    
    * Update finkube_subscription_changes.sql
    
    * more changes
    
    * Update finkube_pipeline_changes.sql
    
    * quick ambiguity fix
    
    * Update finkube_leads.sql
    
    * Fix Signup Channel Attribution
    
    * Update finkube_signups.sql
    
    * CustomerType Added
    
    CustomerType added to Opportunities and Subscriptions. Subscriptions is important since pro will no longer exist in Salesforce so you can't just join SFDC Accounts.
    
    * Update finkube_pipeline.sql
    
    * Adding CustomerType, Fixing Formula Error
    
    * Update finkube_subscriptions.sql
    
    Changing finkube_subscriptions in order to be able to change/iterate in Looker more easily. Commented out/deleted code was causing sporadic increase in Pro when Zuora data is used December onward.

--- models/finkubes/finkube_subscriptions.sql
+++ models/finkubes/finkube_subscriptions.sql
@@ -17,8 +17,12 @@ select
   , fiscalquarter
   , fiscalyear
   , case when a.ispersonaccount or a.isselfserve or a.ispersonaccount is null then 'Vidyard Pro' else a.employeesegment end as customertype
-  , case when yearmonth <= '2020-11' then nvl(sarr,0) else nvl(zarr,0) end as arr
-   , case when yearmonth <= '2020-11' then nvl(sarr/12,0) else nvl(zarr/12,0) end as mrr
+--  , case when yearmonth <= '2020-11' then nvl(sarr,0) else nvl(zarr,0) end as arr
+--   , case when yearmonth <= '2020-11' then nvl(sarr/12,0) else nvl(zarr/12,0) end as mrr
+  , nvl(sarr,0) as salesforcearr
+  , nvl(sarr,0)/12 as salesforcemrr
+  , nvl(zarr,0) as zuoraarr
+  , nvl(zarr,0)/12 as zuoramrr
 from zuora
 full outer join sfdc using (accountid, region, idtype, yearmonth, yearmonthvalue, fiscalyearmonth, fiscalquarter, fiscalyear)
 left join {{ref('tier2_salesforce_account')}} a using (accountid)
