d2285511b65b944069f1273c740b70dafe5128bd
isaac.wu@vidyard.com <isaac.wu@vidyard.com>
2022-01-05 22:01:35 +0000

    add stg table to ad_platforms

--- models/staging/ad_platforms/stg_googlead_campaign_history.sql
+++ models/staging/ad_platforms/stg_googlead_campaign_history.sql
@@ -3,7 +3,10 @@ SELECT
     ch.name as campaignName,
     ch.start_date as startDate,
     ch.end_date as endDate,
-    ch.campaign_trial_type as trailType
+    ch.campaign_trial_type as trailType,
+    ch.base_campaign_id as basecampaignId,
+    ch.advertising_channel_type as channelType,
+    ch.tracking_url_template as trackingTemplate
     
 FROM 
 	{{ source('google_adwords', 'campaign_history') }} as ch
