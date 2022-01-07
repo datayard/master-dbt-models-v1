3e8b20defc262743f4b4814b653fef517d0cf0e6
Gabe Romero <58733497+GabeRomeroVY@users.noreply.github.com>
2022-01-07 09:07:47 -0600

    Dev dbt gabe2 (#405)
    * fixing the build
    
    * reorganizing the folders
    
    * turning finkubes into tables
    
    * Splitting out the salesforce_syncs table on sub-tables
    
    * adding tier2 ctas
    
    * adding tier 2 CTAS

---
+++ models/marts/tier2/tier2_vidyard_ctas.sql
@@ -0,0 +1,55 @@
+WITH
+  total_ctas as (
+    SELECT
+      vyorgs.accountId,
+      COUNT (DISTINCT vyevents.eventID) as cta
+    FROM
+        {{ ref('stg_vidyard_events') }} as vyevents
+    JOIN
+        {{ ref('stg_vidyard_organizations') }} as vyorgs
+    ON
+      vyevents.organizationID = vyorgs.organizationID
+    WHERE
+      true
+    GROUP BY
+      vyorgs.accountId
+    ),
+   
+  ctas_applied as (
+
+    SELECT
+      vyorgs.accountId as accountid,
+      COUNT (distinct vyeventsjoins.eventID) as cta_applied
+    FROM
+      {{ ref('stg_vidyard_events') }} as vyevents
+    JOIN
+      {{ ref('stg_vidyard_event_joins') }} as vyeventsjoins
+    ON
+      vyevents.eventID = vyeventsjoins.eventID
+    JOIN
+      {{ ref('stg_vidyard_organizations') }} as vyorgs
+    ON
+      vyevents.organizationID = vyorgs.organizationID
+    WHERE
+      true
+    GROUP BY
+      vyorgs.accountId
+)
+
+SELECT  
+  vyorgs.accountId,
+  total_ctas.cta,
+  ctas_applied.cta_applied
+FROM
+  total_ctas
+JOIN
+    {{ ref('stg_vidyard_organizations') }} as vyorgs
+ON
+  total_ctas.accountId = vyorgs.organizationID
+LEFT JOIN
+  ctas_applied
+ON
+  vyorgs.accountId = ctas_applied.accountid
+WHERE
+  --true
+  vyorgs.accountId = '12449'
