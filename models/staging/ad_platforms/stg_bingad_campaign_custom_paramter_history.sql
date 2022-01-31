8bd8bbd9a193f7681185e9d5f7b0db789d386e3b
Suresh Subramani <suresh.subramani@vidyard.com>
2022-01-31 16:33:35 +0000

    trivial commit

--- models/staging/ad_platforms/stg_bingad_campaign_custom_paramter_history.sql
+++ models/staging/ad_platforms/stg_bingad_campaign_custom_paramter_history.sql
@@ -2,5 +2,6 @@ SELECT
     cp.campaign_id as campaignId,
     cp.key as parameterKey,
     cp.value as parameterValue
+    
 FROM 
 	{{ source('bingads', 'campaign_custom_parameter_history') }} as cp
