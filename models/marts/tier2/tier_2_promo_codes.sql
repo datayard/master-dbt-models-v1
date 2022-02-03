46bd80cf84c3c30ec8a1c73a8d6790be74cb46ea
David Brunsting <89867262+davidbrunsting1@users.noreply.github.com>
2022-02-03 16:27:10 -0500

    David (#478)
    * added tier2_vidyard_ livestream table
    
    * Added  rpc.effectivestartdate and rpc.effectiveenddate to zuora
    
    * Added probability to stg_opportunity and tier2_opportunity
    
    * changes to tier2_accounts_on_trial and tier2_promo_codes

--- models/marts/tier2/tier_2_promo_codes.sql
+++ models/marts/tier2/tier_2_promo_codes.sql
@@ -9,7 +9,7 @@ SELECT
     vypa.userid,
     vypa.organizationid,
     zrp.name as promo,
-    MIN (zs.createddate) as promoStartDate
+    MIN (zs.subscriptionstartdate) as promoStartDate
     /*CASE WHEN zrp.name like 'First Month Free' THEN DATE_PART(month,zs.createddate) + 1
         else getdate()
     END AS promoEndDate*/
@@ -46,6 +46,9 @@ WHERE
     AND (
         zrp.productrateplanid LIKE '2c92a0107a187273017a1a86987d3717' --First Month Free
         OR zrp.productrateplanid LIKE '2c92a0fe7ac84f2c017ae3ed410a3631' --Three Months Free
+        or zrp.productrateplanid like '2c92a00f7c3056f9017c31f6135f64c2' --First Month Free - Advisor Group
+        or zrp.productrateplanid like '2c92a00f7420c37601742288406615ab' --12 month discount
+        or zrp.productrateplanid like '2c92a0117ac85d21017ace8398e30b63' -- 100% Annual Discount
     )
 GROUP BY
     1,
@@ -62,6 +65,7 @@ GROUP BY
         , CASE
             WHEN promo like 'First Month Free' THEN add_months(promoStartDate, 1)
             WHEN promo like 'Three Months Free' THEN add_months(promoStartDate, 3)
+            WHEN promo like '12 Month Discount' or promo like '100% Annual Discount' then add_months(promoStartDate, 12)
             else getdate()
           END AS promoEndDate
         , datediff(day, getdate(), promoEndDate) as days_remaining
