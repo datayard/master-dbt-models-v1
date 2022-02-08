5c459786c90bad09b0af35e4c4418d65e097b087
Suresh Subramani <suresh.subramani@vidyard.com>
2022-02-08 15:43:52 +0000

    test commit

--- models/staging/ad_platforms/stg_bingad_campaign_history.sql
+++ models/staging/ad_platforms/stg_bingad_campaign_history.sql
@@ -2,7 +2,7 @@ SELECT
     ch.id as campaignId,
     ch.name as campaignName,
     ch.type as campaignType,
-    ch.tracking_template as trackingTemplate
-
+    ch.tracking_template as trackingTemplate,
+    ch.type as test_column
 FROM 
 	{{ source('bingads', 'campaign_history') }} as ch
