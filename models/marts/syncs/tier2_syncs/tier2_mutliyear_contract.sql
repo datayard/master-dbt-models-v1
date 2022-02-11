7c85feec4afccd6c81ae8cb36f8f8838c2b33368
Gabe Romero <gabe.romero@vidyard.com>
2022-02-11 19:58:47 +0000

    migrating the multi-year contract calculation

---
+++ models/marts/syncs/tier2_syncs/tier2_mutliyear_contract.sql
@@ -0,0 +1,40 @@
+with
+  opps as (
+    select
+      a.accountid,
+      a.accountname,
+      a.accounttypee,
+      o.opportunityname,
+      o.contractstartdate,
+      o.contractenddate,
+      o.termlength,
+      row_number() over(partition by o.accountid order by o.termlength desc)
+    from
+        --{{ ref('stg_salesforce_account') }} a
+        dbt_vidyard_master.stg_salesforce_account a
+    left join
+        --{{ ref('stg_salesforce_opportunity') }} o
+        dbt_vidyard_master.stg_salesforce_opportunity o
+    on
+        o.accountid = a.accountid
+    where
+      o.iswon is true
+      and o.contractenddate >= current_date
+  )
+
+select
+  accountid,
+  accountname,
+  accounttypee,
+  case
+    when termlength >= 729
+      then 1
+    else 0
+  end as multi_year
+from
+  opps
+where
+  row_number = 1
+order by
+  4 desc
+  , 2
