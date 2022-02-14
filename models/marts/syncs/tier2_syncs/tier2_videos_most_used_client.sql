3cd2150a9e135bd87aea06b3f2f7622bab7762f5
Gabe Romero <gabe.romero@vidyard.com>
2022-02-14 16:42:13 +0000

    Adding the videos in most used client field in the table

--- models/marts/syncs/tier2_syncs/tier2_videos_most_used_client.sql
+++ models/marts/syncs/tier2_syncs/tier2_videos_most_used_client.sql
@@ -56,7 +56,8 @@ SELECT
     	WHEN muc.most_frequent_client LIKE 'prod.outreach.io'
     		then 'Outreach'
   ELSE muc.most_frequent_client
-END AS client_name
+END AS client_name,
+  muc.videos_from_client  + CAST(' in ' AS varchar) + muc.videos_from_client as videos_in_most_used_client
 FROM
 	most_used_clients as muc
 WHERE
