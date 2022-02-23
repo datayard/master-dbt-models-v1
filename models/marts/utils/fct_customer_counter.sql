4382157446fa0e9d87eb64de0c8071f22b9cca04
gleevidyard <gladwin.lee@vidyard.com>
2022-02-23 17:05:50 -0500

    changed fct customer count

--- models/marts/utils/fct_customer_counter.sql
+++ models/marts/utils/fct_customer_counter.sql
@@ -1,59 +1,11 @@
-WITH pro_acccounts as (
-    SELECT
-        'Self-serve' as accounttype,
-        COUNT (distinct Pro_accounts.activeproaccounts) as accounts
-    FROM
-        {{ ref('fct_active_pro_subscriptions') }} as Pro_accounts
-        --dbt_vidyard_master.fct_active_pro_subscriptions as Pro_accounts
-    WHERE
-          Pro_accounts.name NOT LIKE 'Free Forever'
-    ),
+select case
+        when sku = 'SS-010' then 'Pro Self Serve'
+        when sku = 'SKU-00000009' or sku = 'SKU-00000020' then 'Enterprise Self Serve'
+       end as accounttype,
+       count(distinct accountid)
+-- from dbt_vidyard_master.kube_zuora_ss_subscriptions
+from {{ ref('kube_zuora_ss_subscriptions') }}
+where promocodebillthrough and trailbillthrough and trailbillthroughnextday and sublongerthenoneday
+group by 1
 
-    commercial_accounts as (
-    SELECT
-        'commercial_accounts' as accounttype,
-        COUNT (distinct account.accountid) as accounts
-    FROM
-        {{ ref('stg_salesforce_account') }} as account
-        --dbt_vidyard_master.stg_salesforce_account as account
-    WHERE
-        account.accounttype LIKE 'Customer'
-        AND account.employeesegment LIKE 'Commercial'
-        AND account.ispersonaccount = false
-    ),
 
-    emerging_accounts as (
-    SELECT
-        'emerging_accounts' as accounttype,
-        COUNT (distinct account.accountid) as accounts
-    FROM
-        {{ ref('stg_salesforce_account') }} as account
-        --dbt_vidyard_master.stg_salesforce_account as account
-    WHERE
-        account.accounttype LIKE 'Customer'
-        AND account.employeesegment NOT LIKE 'Commercial'
-        AND account.ispersonaccount = false
-    )
-
-SELECT
-    *
-FROM
-    (
-        SELECT
-            accounttype,
-            accounts
-        FROM pro_acccounts
-        UNION ALL
-        SELECT
-            accounttype,
-            accounts
-        FROM commercial_accounts
-        UNION ALL
-        SELECT
-            accounttype,
-            accounts
-        FROM emerging_accounts
-    )
-GROUP BY
-    accounttype,
-    accounts
