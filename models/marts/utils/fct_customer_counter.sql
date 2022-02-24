0f79aee97dddc4734754a9759484ad1eb05cf402
gleevidyard <gladwin.lee@vidyard.com>
2022-02-24 10:46:22 -0500

    revert fct_Customer_counts

--- models/marts/utils/fct_customer_counter.sql
+++ models/marts/utils/fct_customer_counter.sql
@@ -1,17 +1,59 @@
-select case
-        when z.sku = 'SS-010' then 'Pro Self Serve'
-        when z.sku = 'SKU-00000009' or z.sku = 'SKU-00000020' then 'Enterprise Self Serve'
-       end as accounttype,
-       count(distinct z.accountid)
--- from dbt_vidyard_master.kube_zuora_ss_subscriptions z
-from {{ ref('kube_zuora_ss_subscriptions') }} z
--- left join dbt_vidyard_master.tier2_vidyard_user_details u on u.organizationid = z.vidyardaccountid
-left join {{ ref('tier2_vidyard_user_details') }} u on u.organizationid = z.vidyardaccountid
-where z.promocodebillthrough
-  and z.trailbillthrough
-  and z.trailbillthroughnextday
-  and z.sublongerthenoneday
-  and u.classification != 'enterprise user'
-group by 1
+WITH pro_acccounts as (
+    SELECT
+        'Self-serve' as accounttype,
+        COUNT (distinct Pro_accounts.activeproaccounts) as accounts
+    FROM
+        {{ ref('fct_active_pro_subscriptions') }} as Pro_accounts
+        --dbt_vidyard_master.fct_active_pro_subscriptions as Pro_accounts
+    WHERE
+          Pro_accounts.name NOT LIKE 'Free Forever'
+    ),
 
+    commercial_accounts as (
+    SELECT
+        'commercial_accounts' as accounttype,
+        COUNT (distinct account.accountid) as accounts
+    FROM
+        {{ ref('stg_salesforce_account') }} as account
+        --dbt_vidyard_master.stg_salesforce_account as account
+    WHERE
+        account.accounttype LIKE 'Customer'
+        AND account.employeesegment LIKE 'Commercial'
+        AND account.ispersonaccount = false
+    ),
 
+    emerging_accounts as (
+    SELECT
+        'emerging_accounts' as accounttype,
+        COUNT (distinct account.accountid) as accounts
+    FROM
+        {{ ref('stg_salesforce_account') }} as account
+        --dbt_vidyard_master.stg_salesforce_account as account
+    WHERE
+        account.accounttype LIKE 'Customer'
+        AND account.employeesegment NOT LIKE 'Commercial'
+        AND account.ispersonaccount = false
+    )
+
+SELECT
+    *
+FROM
+    (
+        SELECT
+            accounttype,
+            accounts
+        FROM pro_acccounts
+        UNION ALL
+        SELECT
+            accounttype,
+            accounts
+        FROM commercial_accounts
+        UNION ALL
+        SELECT
+            accounttype,
+            accounts
+        FROM emerging_accounts
+    )
+GROUP BY
+    accounttype,
+    accounts
