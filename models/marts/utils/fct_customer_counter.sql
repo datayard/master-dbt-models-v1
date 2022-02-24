22332bec162bb0b77bfa0702d855c302b8112d8b
gleevidyard <gladwin.lee@vidyard.com>
2022-02-23 21:47:05 -0500

    fct customer count change

--- models/marts/utils/fct_customer_counter.sql
+++ models/marts/utils/fct_customer_counter.sql
@@ -1,11 +1,17 @@
 select case
-        when sku = 'SS-010' then 'Pro Self Serve'
-        when sku = 'SKU-00000009' or sku = 'SKU-00000020' then 'Enterprise Self Serve'
+        when z.sku = 'SS-010' then 'Pro Self Serve'
+        when z.sku = 'SKU-00000009' or z.sku = 'SKU-00000020' then 'Enterprise Self Serve'
        end as accounttype,
-       count(distinct accountid)
--- from dbt_vidyard_master.kube_zuora_ss_subscriptions
+       count(distinct z.accountid)
+-- from dbt_vidyard_master.kube_zuora_ss_subscriptions z
 from {{ ref('kube_zuora_ss_subscriptions') }}
-where promocodebillthrough and trailbillthrough and trailbillthroughnextday and sublongerthenoneday
+-- left join dbt_vidyard_master.tier2_vidyard_user_details u on u.organizationid = z.vidyardaccountid
+left join {{ ref('tier2_vidyard_user_details') }} u on u.organizationid = z.vidyardaccountid
+where z.promocodebillthrough
+  and z.trailbillthrough
+  and z.trailbillthroughnextday
+  and z.sublongerthenoneday
+  and u.classification != 'enterprise user'
 group by 1
 
 
