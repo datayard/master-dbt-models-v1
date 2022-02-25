acfaafe0ab0e75b7557d26f54789ef2a55acae31
gleevidyard <gladwin.lee@vidyard.com>
2022-02-25 14:39:46 -0500

    added comercial accounts and emerging. Changed titles of pro and enterprise ss

--- models/marts/utils/fct_customer_counter.sql
+++ models/marts/utils/fct_customer_counter.sql
@@ -1,3 +1,28 @@
+
+SELECT
+    'commercial_accounts' as accounttype,
+    COUNT (distinct account.accountid) as accounts
+FROM
+    {{ ref('stg_salesforce_account') }} as account
+    --dbt_vidyard_master.stg_salesforce_account as account
+WHERE
+    account.accounttype LIKE 'Customer'
+    AND account.employeesegment LIKE 'Commercial'
+    AND account.ispersonaccount = false
+UNION
+
+SELECT
+    'emerging_accounts' as accounttype,
+    COUNT (distinct account.accountid) as accounts
+FROM
+    {{ ref('stg_salesforce_account') }} as account
+    --dbt_vidyard_master.stg_salesforce_account as account
+WHERE
+    account.accounttype LIKE 'Customer'
+    AND account.employeesegment NOT LIKE 'Commercial'
+    AND account.ispersonaccount = false
+UNION
+
 select case
         when z.sku = 'SS-010' then 'Pro Self Serve'
         when z.sku = 'SKU-00000009' or z.sku = 'SKU-00000020' then 'Enterprise Self Serve'
@@ -12,5 +37,5 @@ and z.trailbillthrough
 and z.trailbillthroughnextday
 and z.sublongerthenoneday
 -- and u.classification != 'enterprise user'
-and (z.subscriptionenddate is null or z.subscriptionenddate < getdate())
+and (z.subscriptionenddate is null or z.subscriptionenddate > getdate())
 group by 1
