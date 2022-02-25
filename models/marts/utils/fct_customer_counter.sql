ea918d282f76e0c513111e93e98ad4f8b6a6cb92
gleevidyard <gladwin.lee@vidyard.com>
2022-02-25 15:26:45 -0500

    fixed subscriptionenddate filter

--- models/marts/utils/fct_customer_counter.sql
+++ models/marts/utils/fct_customer_counter.sql
@@ -1,41 +1,35 @@
-
-SELECT
-    'commercial_accounts' as accounttype,
-    COUNT (distinct account.accountid) as accounts
-FROM
-    {{ ref('stg_salesforce_account') }} as account
+SELECT '' commercial_accounts'' as accounttype, COUNT(distinct account.accountid) as accounts
+FROM {{ ref(''stg_salesforce_account'') }} as account
     --dbt_vidyard_master.stg_salesforce_account as account
 WHERE
-    account.accounttype LIKE 'Customer'
-    AND account.employeesegment LIKE 'Commercial'
-    AND account.ispersonaccount = false
+    account.accounttype LIKE '' Customer ''
+  AND account.employeesegment LIKE '' Commercial ''
+  AND account.ispersonaccount = false
 UNION
 
-SELECT
-    'emerging_accounts' as accounttype,
-    COUNT (distinct account.accountid) as accounts
-FROM
-    {{ ref('stg_salesforce_account') }} as account
+SELECT '' emerging_accounts'' as accounttype, COUNT(distinct account.accountid) as accounts
+FROM {{ ref(''stg_salesforce_account'') }} as account
     --dbt_vidyard_master.stg_salesforce_account as account
 WHERE
-    account.accounttype LIKE 'Customer'
-    AND account.employeesegment NOT LIKE 'Commercial'
-    AND account.ispersonaccount = false
+    account.accounttype LIKE '' Customer ''
+  AND account.employeesegment NOT LIKE '' Commercial ''
+  AND account.ispersonaccount = false
 UNION
 
 select case
-        when z.sku = 'SS-010' then 'pro_self_serve'
-        when z.sku = 'SKU-00000009' or z.sku = 'SKU-00000020' then 'enterprise_self_serve'
-       end as accounttype,
+           when z.sku = ''SS-010'' then ''pro_self_serve''
+           when z.sku = ''SKU-00000009'' or z.sku = '' SKU-00000020'' then ''enterprise_self_serve''
+end as accounttype,
        count(distinct z.accountid)
 -- from dbt_vidyard_master.kube_zuora_ss_subscriptions z
-from {{ ref('kube_zuora_ss_subscriptions') }} z
+from {{ ref(''kube_zuora_ss_subscriptions'') }} z
 -- left join dbt_vidyard_master.tier2_vidyard_user_details u on u.organizationid = z.vidyardaccountid
-left join {{ ref('tier2_vidyard_user_details') }} u on u.organizationid = z.vidyardaccountid
+left join {{ ref(''tier2_vidyard_user_details'') }} u on u.organizationid = z.vidyardaccountid
 where z.promocodebillthrough
 and z.trailbillthrough
 and z.trailbillthroughnextday
 and z.sublongerthenoneday
--- and u.classification != 'enterprise user'
-and (z.subscriptionenddate is null or z.subscriptionenddate > getdate())
+-- and u.classification != ''enterprise user''
+and (z.subscriptionenddate is null or z.subscriptionenddate >= (DATE(DATEADD(minute,0, DATE_TRUNC(''minute'', GETDATE())))))
 group by 1
+
