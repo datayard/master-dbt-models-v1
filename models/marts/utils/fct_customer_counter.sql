1b51b48c88a56f79a256c928e233f876986d3305
gleevidyard <gladwin.lee@vidyard.com>
2022-02-28 13:27:31 -0500

    added enterprise query from david, fixed pro ss count

--- models/marts/utils/fct_customer_counter.sql
+++ models/marts/utils/fct_customer_counter.sql
@@ -23,8 +23,23 @@ WHERE
     AND account.ispersonaccount = false
 UNION
 
+SELECT
+    'Enterprise' as accounttype,
+    SUM (vidyard_allotments.allotmentlimit) as accounts
+FROM
+    dbt_vidyard_master.tier2_vidyard_allotments  AS vidyard_allotments
+JOIN
+    dbt_vidyard_master.stg_salesforce_account as sfdcact
+ON
+    vidyard_allotments.accountid = sfdcact.vidyardaccountid
+WHERE
+    vidyard_allotments.allotmenttypeid = 1
+    AND (sfdcact.accounttype like 'Customer'
+        OR sfdcact.accounttype like 'Sub-Account')
+
+union
 select case
-        when z.sku = 'SS-010' then 'pro_self_serve'
+        when z.sku = 'SS-010'and u.classification != 'enterprise user' then 'pro_self_serve'
         when z.sku = 'SKU-00000009' or z.sku = 'SKU-00000020' then 'enterprise_self_serve'
        end as accounttype,
        count(distinct z.accountid)
@@ -36,7 +51,6 @@ where z.promocodebillthrough
 and z.trailbillthrough
 and z.trailbillthroughnextday
 and z.sublongerthenoneday
--- and u.classification != 'enterprise user'
 and (z.subscriptionenddate is null or z.subscriptionenddate >= (DATE(DATEADD(minute,0, DATE_TRUNC('minute', GETDATE())))))
 group by 1
 
